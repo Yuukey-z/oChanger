cc4695f3a4b4594548a0908d2ff778898e56f7cc
Yuukey-z <rizkymaouu@gmail.com>
2026-02-27 23:07:21 +0800

    Creation

---
+++ src/main/java/me/kayzhng/ochanger/Listeners/KnockBackModifier.java
@@ -0,0 +1,4 @@
+package me.kayzhng.ochanger.Listeners;
+
+public class KnockBackModifier {
+}
---
+++ src/main/java/me/kayzhng/ochanger/OChanger.java
@@ -0,0 +1,13 @@
+package me.kayzhng.ochanger;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class OChanger extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+}
